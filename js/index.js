e8ba9515afa457883d848e7a7d7581b02281835b
AyeChanHmweKyu <ayechanhmwekyu@gmail.com>
2023-05-12 20:29:30 +0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,11 @@
+function onPackagesClick() {
+    document.getElementById('packages').scrollIntoView({ behavior: 'smooth' });
+}
+
+function onAboutClick() {
+    document.getElementById('about').scrollIntoView({ behavior: 'smooth' });
+}
+
+function onContactClick() {
+    document.getElementById('contact-us').scrollIntoView({ behavior: 'smooth' });
+}
